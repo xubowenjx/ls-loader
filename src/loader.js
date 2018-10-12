70640fc32e5c5bd0240f6ba6656359a520fe6d23
xubowenjx <286129936@qq.com>
2018-10-12 10:15:29 +0800

    ignore \\

--- src/loader.js
+++ src/loader.js
@@ -4,10 +4,11 @@ module.exports = function(source) {
   //let prepath = options.prepath
   this.cacheable()
   let newSource = source
-  var reg = /(\[.+\]\(.+\.(doc[x]?)(#.+)?\))/g
+  var reg = /((\\)?\[.+\](\\)?\(.+\.(doc[x]?)(#.+)?\))/g
   newSource = newSource.replace(reg, function(arg1) {
-    return arg1.replace(/\s+/g, '-').replace(/doc[x]?/g, 'md')
-  })
+    console.log(arg1)
+    return arg1.replace(/\s+/g, '-').replace(/\\/g, '').replace(/doc[x]?/g, 'md')
+  });
 
   return newSource
 }
